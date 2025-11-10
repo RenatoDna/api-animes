2295b61858b0066cda562bad82c44cabf3d59801
renatodna <dna.renato@gmail.com>
2025-11-10 15:17:21 -0300

    fix: correção ajuste anterior

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
-import app from './app.js'; 
-const PORT = process.env.PORT || 3000;
+import Router from 'express';
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+const animeRouter = Router();
+
+const animes = [
+  { id: 1, nome: 'Naruto', genero: 'Ação' },
+  { id: 2, nome: 'One Piece', genero: 'Aventura' },
+  { id: 3, nome: 'Attack on Titan', genero: 'Fantasia' },
+  { id: 4, nome: 'Yugioh', genero: 'Jogo de Tabuleiro' },
+  { id: 5, nome: 'Digimon', genero: 'Aventura' }, 
+  { id: 6, nome: 'Pokemon', genero: 'Aventura' },
+];
+
+
+
+function obterAnimes(req, res) {
+  return res.status(200).json(animes);
+}
+
+function deleteAnime(req, res) {
+  const id = parseInt(req.params.id);
+
+  const index = animes.findIndex((anime) => anime.id === id);
+
+  if (index === -1) {
+    return res.status(404).json({ message: 'Anime não localizado' });
+  }
+
+
+  const removido = animes.splice(index, 1);
+
+  return res.status(200).json({
+    mensagem: "Anime removido com sucesso",
+    removido: removido[0], 
+  });
+}
+
+
+animeRouter.get('/animes', obterAnimes); 
+
+
+animeRouter.delete('/animes/:id', deleteAnime);
+
+export default animeRouter;
